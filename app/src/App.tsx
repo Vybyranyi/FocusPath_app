08bb08d4def572cb0f30dda589d4b1e5305dfb61
Vybyranyi <mariankvit@gmail.com>
2025-08-06 15:20:36 +0300

    added reset styles, typography and color variables

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
+import './assets/styles/resetStyles.scss';
+import './assets/styles/typography.scss';
 
 function App() {
 
   return (
     <>
-
+      <h1>Hello World</h1>
     </>
   )
 }
