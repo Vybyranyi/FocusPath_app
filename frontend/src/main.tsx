ac4ad989e2334e050a82e2f49c4c2e4f1a02f734
Vybyranyi <mariankvit@gmail.com>
2025-08-15 17:52:10 +0300

    Add IconButton component with emoji support and styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { EmojiProvider } from "react-apple-emojis"
+import emojiData from "react-apple-emojis/src/data.json"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <EmojiProvider data={emojiData}>
+      <App />
+    </EmojiProvider>
   </StrictMode>,
 )
