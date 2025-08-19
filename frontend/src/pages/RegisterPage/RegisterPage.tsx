656d43039eac054a985d2c0cd6e6cc875ffe929f
Vybyranyi <mariankvit@gmail.com>
2025-08-19 10:45:48 +0300

    created mobile register page

--- frontend/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -2,20 +2,27 @@ import styles from "@pages/RegisterPage/RegisterPage.module.scss";
 import IconButton from "@components/IconButton/IconButton";
 import Input from "@components/Input/Input";
 import Button from "@components/Button/Button";
+import Header from "@components/Header/Header";
+import arrow_left from "@assets/images/icons/arrow-left.svg";
 
 export default function RegisterPage() {
     return (
-        <div className="container">
-            <div className={styles.header}>
-                <IconButton size="large"></IconButton>
-                <h5>Create Account</h5>
+        <>
+            <Header
+                title="Create Account"
+                leftButtonIcon={<IconButton size='large' icon={arrow_left} />}
+            />
+            <div className="container">
+                <div className={styles.form}>
+                    <Input label="name" placeholder="Enter your name" type="text" />
+                    <Input label="surname" placeholder="Enter your surname" type="text" />
+                    <Input label="Birthdate" placeholder="mm/dd/yyyy" type="date" />
+                </div>
+                <div className={styles.buttonContainer}>
+                    <Button type="primary" size="large" >Next</Button>
+                </div>
             </div>
-            <div className={styles.form}>
-                <Input label="name" placeholder="Enter your name" type="text" />
-                <Input label="surname" placeholder="Enter your surname" type="text" />
-                <Input label="Birthdate" placeholder="mm/dd/yyyy" type="date" />
-            </div>
-            <Button type="primary" size="large" >Next</Button>
-        </div>
+
+        </>
     );
 };
