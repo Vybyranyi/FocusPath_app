0772493f35b10aeae2901227cd8d05f703a14640
Vybyranyi <mariankvit@gmail.com>
2025-09-01 17:15:52 +0300

    added top content block for header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,22 +1,42 @@
 import styles from '@components/Header/Header.module.scss';
 import type { ReactNode } from 'react';
+import { useAppSelector } from "@store/hooks";
+import { Emoji } from 'react-apple-emojis';
 
 export interface IHeaderProps {
     title?: string;
     leftButtonIcon?: ReactNode;
     rightButtonIcon?: ReactNode;
+    topContent?: boolean;
 };
 
 export default function Header(props: IHeaderProps) {
+    const { user } = useAppSelector(state => state.auth);
+
     return (
         <header className={styles.header}>
-            <div className={styles.statusBar}></div>
-            <div className="container">
-                <div className={styles.titleWithButtons}>
-                    {props.leftButtonIcon}
-                    <h5 className={styles.headerTitle}>{props.title}</h5>
-                    {props.rightButtonIcon}
-                </div>
+            <div className={`container ${styles.headerWrapper}`}>
+
+                <div className={styles.statusBar}></div>
+
+                {(props.title || props.leftButtonIcon || props.rightButtonIcon) && (
+                    <div className={styles.titleWithButtonsBlock}>
+                        {props.leftButtonIcon}
+                        <h5 className={styles.headerTitle}>{props.title}</h5>
+                        {props.rightButtonIcon}
+                    </div>
+                )}
+
+                {props.topContent && (
+                    <div className={styles.topContentBlock}>
+                        <div className={styles.topContentText}>
+                            <p className='title'>{`Hi, ${user?.name}`} <Emoji className={styles.emojiHand} name='waving hand' /></p>
+                            <p className={`body-light ${styles.grayText}`}>Let’s make habits together!</p>
+                        </div>
+                        <Emoji className={styles.emojiFace} name='smiling face with halo' />
+                    </div>
+                )}
+
             </div>
         </header>
     )
--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -2,6 +2,9 @@ import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { logout } from "@store/authSlice";
 import { useNavigate } from "react-router";
 import Box from "@components/Box/Box";
+import Header from "@components/Header/Header";
+import IconButton from "@components/IconButton/IconButton";
+import arrow_left from "@assets/images/icons/arrow-left.svg";
 
 export default function Main() {
     const { user, token } = useAppSelector(state => state.auth);
@@ -10,12 +13,22 @@ export default function Main() {
 
     return (
         <>
-        <p>token: {token}</p>
-        <p>user: {JSON.stringify(user)}</p>
-        <button onClick={() => {
-            dispatch(logout());
-            navigate('/login');
-        }}>log out</button>
+            <Header
+                title="Label"
+                leftButtonIcon={
+                    <IconButton
+                        size='large'
+                        icon={arrow_left}
+                    />
+                }
+                topContent
+            />
+            <p>token: {token}</p>
+            <p>user: {JSON.stringify(user)}</p>
+            <button onClick={() => {
+                dispatch(logout());
+                navigate('/login');
+            }}>log out</button>
         </>
     )
 }
