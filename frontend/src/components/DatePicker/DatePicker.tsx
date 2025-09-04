622107ed8909020ff9c93a3275d6e4c76440bdca
Vybyranyi <mariankvit@gmail.com>
2025-09-04 16:01:19 +0300

    added DatePicker component

---
+++ frontend/src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,25 @@
+import styles from '@components/DatePicker/DatePicker.module.scss';
+
+export interface IDatePicker {
+    date: Date | string | number;
+    active?: boolean;
+    onClick?: () => void;
+};
+
+const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+export default function DatePicker(props: IDatePicker) {
+    const parsedDate = new Date(props.date);
+    const day = parsedDate.getDate();
+    const weekday = weekdays[parsedDate.getDay()];
+
+    return (
+        <div
+            className={`${styles.datePicker} ${props.active ? styles.active : ""}`}
+            onClick={props.onClick}
+        >
+            <h6 className={styles.day}>{day}</h6>
+            <div className={`chip ${styles.weekday}`}>{weekday}</div>
+        </div>
+    )
+}
--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router";
 // import arrow_left from "@assets/images/icons/arrow-left.svg";
 import AppBarMobile from "@components/AppBarMobile/AppBarMobile";
 import AppBarDecktop from "@components/AppBarDecktop/AppBarDecktop";
+import DatePicker from "@components/DatePicker/DatePicker";
 
 export default function Main() {
     const dispatch = useAppDispatch();
@@ -32,6 +33,7 @@ export default function Main() {
             }}>log out</button> */}
             <AppBarMobile />
             <AppBarDecktop />
+            <DatePicker date='09-03-25' active />
         </>
     )
 }
