0d129759a60db3e46e62595e71617f18cd4dc3a2
Vybyranyi <mariankvit@gmail.com>
2025-10-27 16:57:19 +0200

    enhance AppBarMobile with navigation to CreateHabit and update styles in DatePicker and WeekDatePicker for better responsiveness

--- frontend/src/components/AppBarMobile/AppBarMobile.tsx
+++ frontend/src/components/AppBarMobile/AppBarMobile.tsx
@@ -2,9 +2,11 @@ import styles from '@components/AppBarMobile/AppBarMobile.module.scss';
 import MenuButton from '@components/MenuButton/MenuButton';
 import plus from '@assets/images/icons/plus.svg';
 import { useMediaQuery } from "react-responsive";
+import { useNavigate } from 'react-router';
 
 export default function AppBarMobile() {
     const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+    const navigate = useNavigate();
 
     const unavailablePage = (): void => {
         alert('Unavailable Page');
@@ -16,7 +18,10 @@ export default function AppBarMobile() {
             <menu className={styles.mobileAppBar}>
                 <MenuButton icon='home' active onClick={unavailablePage}/>
                 <MenuButton icon='explore' onClick={unavailablePage}/>
-                <button className={styles.addButton}>
+                <button
+                    className={styles.addButton}
+                    onClick={() => navigate('/createhabit')}
+                >
                     <img src={plus} alt="add button" />
                 </button>
                 <MenuButton icon='activity' dot onClick={unavailablePage}/>
